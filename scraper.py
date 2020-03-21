c98085f3b43e2991b54ed236f261bc101d772ddc
Logan Cooper <lcooper@forio.com>
2020-03-21 13:25:01 -0700

    Add type hints, docstrings, and refactor some of the scraping functions (#3)
    * type-hints Add type hints for existing functions
    
    * type-hints Refactor scraping into own function
    
    * type-hints Have scraper take a scraping function as an argument
    
    * type-hints Add more docstrings
    
    * type-hints Add shebang
    
    * type-hints Add headers file

--- scraper.py
+++ scraper.py
@@ -1,19 +1,24 @@
+#!/usr/bin/env python3
 from bs4 import BeautifulSoup
+from typing import Tuple, Dict, Callable
 import requests
 import re
 import pandas as pd
 import datetime
 
-def get_html(url):
+def get_html(url: str) -> BeautifulSoup:
     """
-    Takes in a url string and returns a BeautifulSoup object representing the page
+    Takes in a url string and returns a BeautifulSoup object
+    representing the page
     """
     page = requests.get(url)
     return BeautifulSoup(page.content, 'html.parser')
 
-def find_tags(soup):
+def find_tags_sf(soup: BeautifulSoup) -> Tuple[int, int, str]:
     """
-    Takes in a BeautifulSoup object and returns a tuple of the number of cases (int), the number of deaths (int) and the time that the data was updated(str)
+    Takes in a BeautifulSoup object and returns a tuple of the number
+    of cases (int), the number of deaths (int) and the time that the
+    data was updated(str)
     """
     helpful_links_box = soup.find(id='helpful-links')
 
@@ -30,23 +35,24 @@ def find_tags(soup):
     time_updated = re.match(time_regex, time_html.text).group(1)
     return (num_cases, num_deaths, time_updated)
 
-def format_single_digits(number):
+def format_single_digits(number: int) -> str:
     """
-    Adds a leading zero to one digit numbers to make them line up with the existing data
+    Adds a leading zero to one digit numbers to make them line
+    up with the existing data
     """
     if number < 10:
         return "0{0}".format(number)
     else:
-        return number
+        return str(number)
 
-def format_year(year):
+def format_year(year: int) -> int:
     """
     Turns a year into its short form e.g. 2020 -> 20
     """
     # get the short form of the year
     return year - 2000
 
-def gen_date():
+def gen_date() -> str:
     """
     Generates today's date in MM/DD/YY format
     """
@@ -57,18 +63,19 @@ def gen_date():
 
     return "{0}/{1}/{2}".format(month, day, year)
 
-def format_time(time_string):
+def format_time(timestamp: str) -> str:
     """
     Formats time in the format HH:MM:SS AM/PM
     """
-    time, suffix = time_string.split(' ')
+    time, suffix = timestamp.split(' ')
     if len(time) == 4: # time is less than 10:00 w/ length 5
         time = '0{0}'.format(time)
     return '{0}:00 {1}'.format(time, suffix)
 
-def gen_new_row_dict(dataframe, num_cases, num_deaths, time_updated):
+def gen_new_row_dict(dataframe: pd.DataFrame, num_cases: int, num_deaths: int, time_updated: str) -> Dict:
     """
-    Generates a new row for a dataframe in dict format and calculates the new cases and deaths for the new data
+    Generates a new row for a dataframe in dict format and calculates
+    the new cases and deaths for the new data
     """
     cases_idx = 2
     deaths_idx = 4
@@ -88,11 +95,23 @@ def gen_new_row_dict(dataframe, num_cases, num_deaths, time_updated):
         'state': 'CA'
     }
 
-url = 'https://www.sfdph.org/dph/alerts/coronavirus.asp'
-sfdph_soup = get_html(url)
-cases, deaths, time = find_tags(sfdph_soup)
+def scraper(url: str, existing_data_path: str, data_getter: Callable) -> None:
+    """
+    Puts together the other functions in this file to add new data from the
+    specified URL (gathered according to data_getter) to the specified CSV
+    """
+    soup = get_html(url)
+    print('Fetchind data from {0}'.format(url))
+    cases, deaths, time = data_getter(soup)
+    covid_data = pd.read_csv(existing_data_path, dtype={'total_positive_cases': 'Int64', 'total_deaths': 'Int64'})
+    new_row = gen_new_row_dict(covid_data, cases, deaths, time)
+    covid_data = covid_data.append(new_row, ignore_index=True)
+    print('Added the following row to {0}:'.format(existing_data_path))
+    print(covid_data.tail(1))
+    covid_data.to_csv(existing_data_path, index=False)
 
-covid_data = pd.read_csv('data/covid_19_sf.csv', dtype={'total_positive_cases': 'Int64', 'total_deaths': 'Int64'})
-new_row = gen_new_row_dict(covid_data, cases, deaths, time)
-covid_data = covid_data.append(new_row, ignore_index=True)
-covid_data.to_csv('data/covid_19_sf.csv', index=False)
+sf_url = 'https://www.sfdph.org/dph/alerts/coronavirus.asp'
+sf_data = 'data/covid_19_sf.csv'
+# allows us to see all the columns of the new row
+pd.set_option('display.max_columns', None)
+scraper(sf_url, sf_data, find_tags_sf)
