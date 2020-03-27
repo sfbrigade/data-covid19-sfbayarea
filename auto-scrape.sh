40f7de8b62c0ed9282414427a59cad5e4c878797
Logan Cooper <lcooper@forio.com>
2020-03-26 18:16:02 -0700

    automation Finish bash script to automate data scraping and committing

--- auto-scrape.sh
+++ auto-scrape.sh
@@ -1,7 +1,10 @@
-git checkout master;
+now=`date`;
+# replaces all spaces and colons in date with dashes
+# to make valid git branch name
+branch_now=`echo "$now" | sed 's/[ :]/-/g'`;
+git checkout -b "data-$branch_now";
 source env/bin/activate;
 python3 scraper.py;
 git add data/covid_19_sf.csv;
-now=`date`;
 git commit -m "Add SF data -- $now";
 git push;
