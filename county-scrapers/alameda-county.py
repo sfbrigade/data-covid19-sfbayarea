cf95d93c9ebb353531e2370f30226aff0c0bcb94
Elaine Laguerta <12928553+elaguerta@users.noreply.github.com>
2020-04-22 13:29:23 -0700

    Fetch Alameda county cases and deaths into pandas object

--- county-scrapers/alameda-county.py
+++ county-scrapers/alameda-county.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 import requests
 import json
+import pandas as pd
 from typing import List, Dict
 from datetime import datetime, timedelta
 
-# .json file endpoints
+# data endpoints
 # Note that we are using numbers for all of Alameda County, including Berkeley
-counts = 'https://services3.arcgis.com/1iDJcsklY3l3KIjE/arcgis/rest/services/COVID_Counts/FeatureServer/0/query?where = 0 % 3D0 & outFields = * & f = pjson'
-age_gender_url = 'https://services3.arcgis.com/1iDJcsklY3l3KIjE/ArcGIS/rest/services/COVID_Gender_age/FeatureServer/0/3/?f=pjson'
-race_ethnicity_url = 'https://services3.arcgis.com/1iDJcsklY3l3KIjE/arcgis/rest/services/Race_Eth2/FeatureServer/0/3/?f=pjson'
+#EL: get just the AC columns in dte order. Return the latest date as the cumulative case numbers.
+counts = 'https://services3.arcgis.com/1iDJcsklY3l3KIjE/arcgis/rest/services/COVID_Counts/FeatureServer/0//query'
+age_gender_url = 'https://services3.arcgis.com/1iDJcsklY3l3KIjE/ArcGIS/rest/services/COVID_Gender_age/FeatureServer/0/3/?f=json'
+race_ethnicity_url = 'https://services3.arcgis.com/1iDJcsklY3l3KIjE/arcgis/rest/services/Race_Eth2/FeatureServer/0/3/?f=json'
 
 def get_json(url, query=''):
     """
+    Helper method.
     Fetches data from url with optional query in JSON format
     and parses it into a dict
     """
@@ -19,13 +22,22 @@ def get_json(url, query=''):
     parsed_json = json.loads(raw_response.content)
     return parsed_json
 
-
-# https: // services3.arcgis.com/1iDJcsklY3l3KIjE/arcgis/rest/services/COVID_Counts/FeatureServer/0/query?select = features & where = 0 = 0 & orderby = ObjectID & outFields = * & f = pjson
 # Confirmed Cases and Deaths
-def get_timeseries() -> Dict:
+def get_timeseries() -> pd.DataFrame:
     """Get a time series of confirmed cases and deaths """
-    return get_json(counts)["features"]
+    param_list = {'where':'0=0', 'resultType': 'none', 'outFields': 'Date,AC_Cases,AC_CumulCases,AC_Deaths,AC_CumulDeaths,ObjectId', 'orderByField': 'Date', 'f': 'json'}
+    response = requests.get(counts, params=param_list)
+    parsed = json.loads(response.content)
+    fields = parsed['fields']
+    features = [obj["attributes"] for obj in parsed['features']]
+    rows = [entry["ObjectId"] for entry in features]
+    cols = [f["name"] for f in parsed['fields']]
+    dframe = pd.DataFrame(data=features, index=rows, columns=cols)
+    return dframe
 
+def get_cases_and_deaths(timeseries):
+    """Get latest date entry for cumulative cases and deaths"""    
+    return timeseries.loc[timeseries.Date.idxmax()]
 
 # Confirmed cases by age and gender
 def get_age_gender_json() -> Dict:
@@ -53,13 +65,13 @@ def get_race_ethnicity_json() -> Dict:
     source = get_json(race_ethnicity_url)["feature"]["attributes"]
     return { col: source[col] for col in source.keys() if col != "Geography" and col != "ObjectId"}
 
-
-
-
 if __name__ == '__main__':
     """ When run as a script, logs grouped data queries to console"""
-    # print("Total cases and deaths: \n", json.dumps(
-    #      get_cases_and_deaths(), indent=4))
+    ac_timeseries = get_timeseries()
+    print("Timeseries cases and deaths: \n", ac_timeseries)
+    ac_cumulative = get_cases_and_deaths(ac_timeseries)
+    print("Cumulative casees and deaths: \n", ac_cumulative)
+
     # print("Cases by age:\n", json.dumps(get_age_json(), indent=4))
     # print("Cases by gender:\n", json.dumps(get_gender_json(), indent=4))
     # print("Cases by race/ethnicity:\n", json.dumps(get_race_ethnicity_json(), indent=4))
