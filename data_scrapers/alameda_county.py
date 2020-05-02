287a615333dc0b5b333298d711495eab642c092b
Elaine Laguerta <12928553+elaguerta@users.noreply.github.com>
2020-05-02 12:46:31 -0700

    Refactor get_timeseries and get_demographics to re-key. Remove DataFrame option.

--- data_scrapers/alameda_county.py
+++ data_scrapers/alameda_county.py
@@ -19,22 +19,22 @@ demographics_meta = 'https://services3.arcgis.com/1iDJcsklY3l3KIjE/arcgis/rest/s
 dashboards = ['https://ac-hcsa.maps.arcgis.com/apps/opsdashboard/index.html#/1e0ac4385cbe4cc1bffe2cf7f8e7f0d9',
               'https://ac-hcsa.maps.arcgis.com/apps/opsdashboard/index.html#/332a092bbc3641bd9ec8373e7c7b5b3d']
 
-def get_county() -> Dict:
-    """Fetch county data into standard .json format"""
+# Load data model template into the 'out' dictionary. 'out' will be global to all methods.
+with open('./data_scrapers/_data_model.json') as template:
+    out = json.load(template)
 
-    # load data model template into a local dictionary
-    with open('./data_scrapers/_data_model.json') as template:
-        out = json.load(template)
-    
+def get_county() -> Dict:
+    """Main method for populating the out dictionary"""
     # populate dataset headers
     out["name"] = "Alameda County"
     out["source_url"] = "https://data.acgov.org/search?source=alameda%20county%20public%20health%20department&tags=covid-19"
     out["meta_from_source"] = get_notes()
 
     # fetch cases metadata, to get the timestamp
-    response = json.loads(requests.get(cases_meta).content)
+    response = requests.get(cases_meta)
     response.raise_for_status()
-    timestamp = response["editingInfo"]["lastEditDate"]
+    cases_header = json.loads(response.text)
+    timestamp = cases_header["editingInfo"]["lastEditDate"]
     # Raise an exception if a timezone is specified. If "dateFieldsTimeReference" is present, we need to edit this scrapr to handle it. 
     # See: https://developers.arcgis.com/rest/services-reference/layer-feature-service-.htm#GUID-20D36DF4-F13A-4B01-AA05-D642FA455EB6
     if "dateFieldsTimeReference" in cases_header["editingInfo"] or "editFieldsInfo" in cases_header:
@@ -45,46 +45,27 @@ def get_county() -> Dict:
 
     # get cases, deaths, and demographics data
     out["series"] = get_timeseries()
-
-
     demo_totals, counts_lt_10 = get_demographics()
+    out["case_totals"], out["death_totals"] = demo_totals["case_totals"], demo_totals["death_totals"]
     out["meta_from_baypd"] = "These datapoints have a value less than 10: " + ", ".join([item for item in counts_lt_10])
-
-    # parse demographics
-    # note: gender does not currently include other genders
-    gender_keys = {"female": "Female", "male": "Male", "unknown": "Unknown_Sex"} # dict of target_label : source_label for re-keying
-    race_keys = {"Latinx/Hispanic": "Hispanic/Latino", "Asian": "Asian", "African_Amer": "African_American/Black", 
-                 "White":"White", "Pacific_Islander": "Pacific_Islander", "Native_Amer": "Native_American", "Multiple_Race": "Multirace", 
-                 "Other": "Other_Race", "Unknown": "Unknown_Race"}
-    
-    # parse gender cases
-    for k,v in gender_keys.items():
-        out["case_totals"]["gender"][k] = demo_totals[v]
-    # parse race cases and deaths
-    for k, v in race_keys.items():
-        out["case_totals"]["race_eth"][k] = demo_totals[v]
-        out["death_totals"]["race_eth"][k] = demo_totals['Deaths\u2014' + v] # dashes are decoded as the escape sequence '\u2014'
-    # get age cases
-    out["case_totals"]["age_group"] = { k:v for k,v in demo_totals.items() if 'Age' in k}
-
-    with open('./county_data/alameda_county.json', 'w', encoding = 'utf-8') as f:
-        json.dump(out, f, ensure_ascii= False, indent=4)
-        
-    return json.dumps(out, indent=4) # for printing to console
+    return out
 
 
 # https: // services3.arcgis.com/1iDJcsklY3l3KIjE/arcgis/rest/services/COVID_Counts/FeatureServer/0/query?select = features & where = 0 = 0 & orderby = ObjectID & outFields = * & f = pjson
 # Confirmed Cases and Deaths
-def get_timeseries(dataframe = False) -> Dict: 
+def get_timeseries() -> Dict: 
     """Fetch daily and cumulative cases and deaths by day
-    Returns the dictionary value for "series": {"cases":[], "deaths":[]} by default.
-    If dataframe set to True, returns a pandas DataFrame."""
+    Returns the dictionary value for "series": {"cases":[], "deaths":[]}.
+    To create a DataFrame from this dictionary, run
+    'pd.DataFrame(get_timeseries())'
+    """
 
     series = {"cases":[], "deaths":[]} # dictionary holding the timeseries for cases and deaths
 
     # query API
     param_list = {'where':'0=0', 'resultType': 'none', 'outFields': 'Date,AC_Cases,AC_CumulCases,AC_Deaths,AC_CumulDeaths', 'outSR': 4326,'orderByField': 'Date', 'f': 'json'}
     response = requests.get(cases_deaths, params=param_list)
+    response.raise_for_status()
     parsed = json.loads(response.content)
     features = [obj["attributes"] for obj in parsed['features']]
     
@@ -93,6 +74,8 @@ def get_timeseries(dataframe = False) -> Dict:
         month, day, year = obj['Date'].split('/')
         if int(month) < 10:
             month = '0' + month
+        if int(day) < 10:
+            day = '0' + day
         obj['Date'] = "{}-{}-{}".format(year, month, day)
 
     # re-key series
@@ -106,51 +89,69 @@ def get_timeseries(dataframe = False) -> Dict:
     
     series["cases"] = [{k: v for k, v in entry.items() if k in case_keys} for entry in re_keyed]
     series["deaths"] = [{k: v for k, v in entry.items() if k in death_keys} for entry in re_keyed]
-    
-    if not dataframe:
-        return series
-    
-    fields = parsed["fields"]
-    cols = [f['name'] for f in parsed['fields']]
-    dframe = pd.DataFrame(data=series, index=rows, columns=cols)
-    return dframe
+
+    return series
 
 def get_notes() -> str:
     """Scrape notes and disclaimers from dashboards."""
     notes = ""
     return notes
 
-def get_demographics(dataframe = False) -> Dict:
+def get_demographics() -> (Dict, List):
     """Fetch cases and deaths by age, gender, race, ethnicity
-    Returns a .json by default. If dataframe set to True, returns a pandas
-    DataFrame."""
+    Returns the dictionary value for {"cases_totals": {}, "death_totals":{}}, as well as a list of 
+    strings describing datapoints that have a value of "<10". 
+    To crete a DataFrame from the dictionary, run 'pd.DataFrame(get_demographics()[0])' 
+    Note that the DataFrame will convert the "<10" strings to NaN. """
+
     param_list = {'where': "Geography='Alameda County'", 'outFields': '*', 'outSR':4326, 'f':'json'}
     response = requests.get(demographics, params=param_list)
+    response.raise_for_status()
     parsed = response.json()
     fields = parsed['fields']
     data = parsed['features'][0]['attributes']
-    #make a list of all datapoints with value '<10'
-    # due to some quirk in the json decoding, dashes are stored as the escape sequence '\u2014'
-    counts_lt_10 = []
-    for key, val in data.items():
-        key = key.replace('\u2014', '-')
-        if val == '<10':
-            counts_lt_10.append(key)
-    if not dataframe:
-        return data, counts_lt_10
 
-    cols = [f['name'] for f in parsed['fields']]
-    dframe = pd.DataFrame(data=data, index=rows, columns=cols)
-    return dframe, counts_lt_10
+    # copy dictionary structure of global 'out' dictionary to local variable
+    demo_totals = { "case_totals": out["case_totals"], "death_totals": out["death_totals"]} 
+
+    # Parse and re-key
+    # note: gender does not currently include other genders
+    # dict of target_label : source_label for re-keying
+    gender_keys = {"female": "Female", "male": "Male", "unknown": "Unknown_Sex"}
+    race_keys = {"Latinx_or_Hispanic": "Hispanic/Latino", "Asian": "Asian", "African_Amer": "African_American/Black",
+                 "White": "White", "Pacific_Islander": "Pacific_Islander", "Native_Amer": "Native_American", "Multiple_Race": "Multirace",
+                 "Other": "Other_Race", "Unknown": "Unknown_Race"}
+
+    # parse gender cases
+    for k, v in gender_keys.items():
+        demo_totals["case_totals"]["gender"][k] = data[v]
+    # parse race cases and deaths
+    for k, v in race_keys.items():
+        demo_totals["case_totals"]["race_eth"][k] = data[v]
+        # dashes are decoded as the escape sequence '\u2014'
+        demo_totals["death_totals"]["race_eth"][k] = data['Deaths\u2014' + v]
+    # get age cases
+    demo_totals["case_totals"]["age_group"] = {
+        k: v for k, v in data.items() if 'Age' in k}
+
+    #make a list of all datapoints with value '<10'
+    counts_lt_10 = []
+    print(demo_totals)
+    for cat, cat_dict in demo_totals.items(): # cases, deaths
+        for group, group_dict in cat_dict.items(): # dictionaries for age, race/eth
+            for key, val in group_dict.items():
+                if val == '<10':
+                    counts_lt_10.append(cat + ' ' + key)
+                else:
+                    try:
+                        int(val)
+                    except ValueError:
+                        raise ValueError(f'Non-integer value for {key}')
+    return demo_totals, counts_lt_10
 
 
 
 
 if __name__ == '__main__':
-    """ When run as a script, logs data to console"""
-    # print(get_county())
-    print(get_notes())
-    # ac_timeseries = get_timeseries()
-    # print("Timeseries cases and deaths: \n", ac_timeseries)
-    # demographics = get_demographics(False)
-    # print("Cases and deaths by demographics: \n", demographics)
+    """ When run as a script, prints the data to stdout"""
+    print(json.dumps(get_county(), indent=4))
