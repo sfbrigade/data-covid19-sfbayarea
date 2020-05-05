6ea27dfcc7f08293052b2bfb3fa3f943dee941a6
tiger135 <vishakh.arora29@gmail.com>
2020-05-05 10:08:23 -0700

    Modifed shell script to automatically pull/push data, changed cronjob accordingly

--- run_scraper.sh
+++ run_scraper.sh
@@ -1,2 +1,8 @@
-source env/bin/activate;
-python3 scraper.py;
+source /Users/vishakh/miniconda3/bin/activate;
+git checkout github-action
+git pull
+python3 scraper.py > data.json;
+now="$(TZ=America/Los_Angeles date -v -1d +'%m/%d/%Y')"
+git add data.json
+git commit -m "Updated data.json for ${now}"
+git push
