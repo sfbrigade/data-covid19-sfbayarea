8de83d370722385cff6172dba30247c02445db47
Elaine Laguerta <12928553+elaguerta@users.noreply.github.com>
2020-05-08 13:13:45 -0700

    Resolve merge conflict markers

--- data_scrapers/san_francisco_county.py
+++ data_scrapers/san_francisco_county.py
@@ -3,15 +3,7 @@ import requests
 import json
 from typing import Dict
 
-<<<<<<< HEAD:data_scrapers/san_francisco_county.py
-<<<<<<< HEAD:data_scrapers/san_francisco_county.py
-# API endpoints
-=======
-#EL: get just the AC columns in dte order. Return the latest date as the cumulative case numbers.
-=======
->>>>>>> Fetch Alameda county cases and deaths into pandas object:county-scrapers/san-francisco-county.py
 # API endpoints 
->>>>>>> Alameda - fetch age, gender, race:county-scrapers/san-francisco-county.py
 age_gender_url = 'https://data.sfgov.org/resource/sunc-2t3k.json'
 race_ethnicity_url = 'https://data.sfgov.org/resource/vqqm-nsqg.json'
 transmission_url = 'https://data.sfgov.org/resource/tvq9-ec9w.json'
