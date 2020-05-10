fdab8a43566f0c02d0dc3733f7120902078554d7
Logan Cooper <lcooper@forio.com>
2020-05-10 15:44:18 -0700

    sonoma Add transmission types

--- data_scrapers/sonoma_county.py
+++ data_scrapers/sonoma_county.py
@@ -23,17 +23,54 @@ def get_source_meta(soup: BeautifulSoup) -> str:
         if el.text == 'Definitions':
             definitions_header = el
     if definitions_header == None:
-        raise FutureWarning('The webpage has changed and the source metadata has moved -- please look at the Sonoma County webpage and locate it, then update the scraper with this information')
+        raise FutureWarning('The source metadata has moved -- please look at the Sonoma County webpage and locate it, then update the scraper with this information')
     definitions_text = definitions_header.find_parent().text
     return definitions_text
 
-model = {
+# def transform_cases(cases_tag: element.Tag) -> List[Dict]:
+#     cases = []
+#     cumul_cases = 0
+#     deaths = []
+#     cumul_deaths = 0
+#     recovered = []
+#     cumul_recovered = 0
+#     rows = cases_tag.findAll('tr')[1:]
+#     for row in rows:
+#         row_cells = row.findAll(['th', 'td'])
+#         date = row_cells[0].text.replace('/', '-')
+#         infected, new_infected, dead, recoveries = [int(el.text) for el in row_cells[1:]]
+#         print(infected)
+#         cumul_cases += new_infected
+#         cases.append({ 'date': date, 'cases': infected, 'cumul_cases': cumul_cases})
+
+def transform_transmission(transmission_tag: element.Tag) -> Dict[str, int]:
+    transmissions = {}
+    rows = transmission_tag.findAll('tr')[1:]
+    # turns the transmission categories on the page into the ones we're using
+    transmission_type_conversion = {'Community': 'community', 'Close Contact': 'from_contact', 'Travel': 'travel', 'Under Investigation': 'unknown'}
+    for row in rows:
+        row_cells = row.findAll(['th', 'td'])
+        type, number, _pct = [el.text for el in row_cells]
+        if type not in transmission_type_conversion:
+            raise FutureWarning('The transmission type {0} was not found in transmission_type_conversion'.format(type))
+        type = transmission_type_conversion[type]
+        transmissions[type] = int(number)
+    return transmissions
+
+    model = {
     'name': 'Sonoma County',
     'update_time': generate_update_time(sonoma_soup),
     'source': url,
-    'meta_from_source': get_source_meta(sonoma_soup)
+    'meta_from_source': get_source_meta(sonoma_soup),
+    'meta_from_baypd': '',
+    'series': {},
+    'case_totals': {
+        'transmission_cat': transform_transmission(source)
+    }
 }
 
-# cases, source, tests, age, sex, region, hospitalized = tables
+try:
+    cases, source, tests, age, sex, region, regions, hospitalized, underlying, symptoms = tables
+except ValueError as e:
+    raise FutureWarning('The number of values on the page has changed -- please ')
 # transform_cases(cases)
-print(get_source_meta(sonoma_soup))
