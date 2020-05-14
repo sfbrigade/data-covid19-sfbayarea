00612986cbcfa48776ad2caa22223d5206b9a367
Rob Brackett <rob@robbrackett.com>
2020-05-13 18:34:12 -0700

    Add RSS foramtting, fixes #42

--- news/feed.py
+++ news/feed.py
@@ -4,10 +4,13 @@ Tools for modeling news feeds and serializing to multiple formats.
 
 from dataclasses import dataclass, field, fields
 from datetime import datetime
+import locale
+from lxml.builder import E
+import lxml.etree as ElementTree
 from typing import Dict, List, Optional, Any
 
 
-def format_datetime(date_obj: datetime) -> str:
+def format_datetime_8601(date_obj: datetime) -> str:
     """
     Get an ISO 8601-formatted string for a datetime, but ensure that UTC is
     represented with the shortened 'Z' form (i.e. W3C-style).
@@ -16,6 +19,17 @@ def format_datetime(date_obj: datetime) -> str:
     if formatted.endswith('+00:00'):
         formatted = formatted[:-6] + 'Z'
     return formatted
+
+
+def format_datetime_2822(date_obj: datetime) -> str:
+    """
+    Get an RFC 2822-formatted string for a datetime.
+    """
+    old = locale.setlocale(locale.LC_ALL)
+    locale.setlocale(locale.LC_ALL, 'C')
+    date = date_obj.strftime('%a, %d %b %Y %H:%M:%S %z')
+    locale.setlocale(locale.LC_ALL, old)
+    return date
 
 
 @dataclass
@@ -35,7 +49,7 @@ class NewsItem:
         return {
             'url': self.url,
             'text': self.title,
-            'date': format_datetime(self.date_published)
+            'date': format_datetime_8601(self.date_published)
         }
 
     def format_json_feed(self) -> Dict:
@@ -47,10 +61,18 @@ class NewsItem:
             value = getattr(self, item_field.name)
             if value:
                 if isinstance(value, datetime):
-                    value = format_datetime(value)
+                    value = format_datetime_8601(value)
                 result[item_field.name] = value
 
         return result
+
+    def format_rss(self) -> ElementTree.Element:
+        return E.item(
+            E('guid', self.id),
+            E('title', self.title),
+            E('link', self.url),
+            E('pubDate', format_datetime_2822(self.date_published))
+        )
 
 
 @dataclass
@@ -66,11 +88,27 @@ class NewsFeed:
     items: List[NewsItem] = field(default_factory=list, init=False)
 
     def format_json_simple(self) -> Dict:
+        """
+        Output this news feed in a simplified JSON format. The output is a
+        JSON-serializable dict; not an actual string.
+
+        Returns
+        -------
+        dict
+        """
         return {
             'newsItems': [item.format_json_simple() for item in self.items]
         }
 
     def format_json_feed(self) -> Dict:
+        """
+        Output this news feed in JSON Feed format (https://jsonfeed.org/). The
+        output is a JSON-serializable dict; not an actual string.
+
+        Returns
+        -------
+        dict
+        """
         if not self.title:
             raise ValueError('You must specify a `title` for this feed')
 
@@ -86,3 +124,47 @@ class NewsFeed:
                 feed[item_field.name] = value
 
         return feed
+
+    # TODO: revisit whether this should really return a string -- when the XML
+    # is serialized to bytes in a file, it should ideally have an XML encoding
+    # declaration at the top, but `ElementTree.tostring` very reasonably won't
+    # output that if you're asking for a string (`encoding='unicode'`) back
+    # instead of bytes (e.g. with `encoding='utf-8'`).
+    def format_rss(self, pretty: bool = True) -> str:
+        """
+        Output this news feed in RSS 2.0 format.
+
+        Parameters
+        ----------
+        pretty
+            Whether to "pretty print" the XML so there's generally one element
+            per line, instead of shoving it all together in the most compact
+            possible representation. (Default: True)
+
+        Returns
+        -------
+        str
+            An XML string. Note this is a *string*, not bytes, so you may want
+            to add an XML encoding header (e.g.
+            `<?xml version="1.0" encoding="UTF-8"?>`) when writing to disk.
+        """
+        if not self.title:
+            raise ValueError('You must specify a `title` for this feed')
+        if not self.home_page_url:
+            raise ValueError('You must specify a `home_page_url` for this feed')
+
+        rss = E.rss(
+            E.channel(
+                E('title', self.title),
+                E('link', self.home_page_url),
+                # Description is required in RSS 2.0, but we don't always
+                # have anything useful to put there.
+                E('description', self.description or ''),
+                *(item.format_rss() for item in self.items)
+            ),
+            {'version': '2.0'}
+        )
+
+        #
+        return ElementTree.tostring(rss, encoding='unicode',
+                                    pretty_print=pretty)
--- scraper_news.py
+++ scraper_news.py
@@ -2,6 +2,7 @@
 import click
 import json
 import news
+from pathlib import Path
 from typing import Tuple
 
 
@@ -13,7 +14,7 @@ COUNTY_NAMES = tuple(news.scrapers.keys())
 @click.argument('counties', metavar='[COUNTY]...', nargs=-1,
                 type=click.Choice(COUNTY_NAMES, case_sensitive=False))
 @click.option('--format', default='json_simple',
-              type=click.Choice(('json_feed', 'json_simple')))
+              type=click.Choice(('json_feed', 'json_simple', 'rss')))
 def main(counties: Tuple[str], format: str) -> None:
     if len(counties) == 0:
         counties = ('san_francisco',)
@@ -24,10 +25,13 @@ def main(counties: Tuple[str], format: str) -> None:
         feed = scraper.scrape()
 
         if format == 'json_simple':
-            data = feed.format_json_simple()
+            data = json.dumps(feed.format_json_simple(), indent=2)
+        elif format == 'json_feed':
+            data = json.dumps(feed.format_json_feed(), indent=2)
         else:
-            data = feed.format_json_feed()
-        print(json.dumps(data, indent=2))
+            data = feed.format_rss()
+
+        print(data)
 
 
 if __name__ == '__main__':
