9893f63c8dac846ec3adad0f7e9b1ec81624ddf3
kwonangela7 <kwonangela7@berkeley.edu>
2020-05-27 00:26:59 -0700

    revised csv parsing logic now that I'm working with a csv_string

--- marin_scraper.py
+++ marin_scraper.py
@@ -8,15 +8,22 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from urllib.parse import unquote_plus
 
-# GOING TO WORK ON: then scrape the metadata, and then figure out how to download the CSVs (seleniuim)
-# throw error when the button isn't correctly pressed
+
 # do a final check 
 def get_county_data():
 	url = 'https://coronavirus.marinhhs.org/surveillance'
-	case_csv = '/Users/angelakwon/Downloads/data-Eq6Es.csv'
+	#case_csv = '/Users/angelakwon/Downloads/data-Eq6Es.csv'
 	"""Main method for populating county data"""
 	with open('/Users/angelakwon/Desktop/data-covid19-sfbayarea/data-model.json') as template:
+		# TO-DO: Need to change the location here to work for anyone..look at other scrapers
 		model = json.load(template)
+
+	csvs = {"cases": "Eq6Es", "deaths": "Eq6Es", "tests": None, "age": "VOeBm", "gender": "FEciW", "race_eth": "aBeEd", "transmission": None}
+	# There are two separate race_eth csvs, but I think I picked the more comprehensive one. 
+	# The other one has id: 6RXFj
+	# TO-DO: Do they have 
+	# NOTE: they used to have a pos/neg test csv, but it seems to be gone now. 
+	# also, their graph doesn't show pos/neg.
 
 	#model['name'] = 
 	#model['update_time'] = 
@@ -24,41 +31,40 @@ def get_county_data():
 	#model['meta_from_source'] = 
 	# make sure to get the comments below the data
 	#model['meta_from_baypd']
-	model["series"]["cases"] = get_case_series(case_csv) 
-	model["series"]["deaths"] =  get_death_series(case_csv)
+	model["series"]["cases"] = get_case_series(csvs["cases"], url) 
+	#model["series"]["deaths"] =  get_death_series(csvs["deaths"], url)
 	#model["series"]["tests"] = get_test_series(case_csv)
 
 	#print(model)
 	#print(get_metadata(url))
-	print(extract_csvs(url))
+	print(model["series"]["cases"])
 
-def extract_csvs(url):
+def extract_csvs(chart_id, url):
 	# div class = "dw-chart-notes"
 	driver = webdriver.Chrome('/Users/angelakwon/Downloads/chromedriver') 
 	# can I leave this blank, will virtual env take care of it?
 	driver.implicitly_wait(30)
 	driver.get(url)
 
-	chart_id = 'tyXjV'
 	frame = driver.find_element_by_css_selector(f'iframe[src^="//datawrapper.dwcdn.net/{chart_id}/"]')
 	driver.switch_to.frame(frame)
 	# Grab the raw data out of the link's href attribute
 	csv_data = driver.find_element_by_class_name('dw-data-link').get_attribute('href')
 	# Switch back to the parent frame to "reset" the context
 	driver.switch_to.parent_frame()
-
+	
 	# Deal with the data
 	if csv_data.startswith('data:'):
-    	media, data = csv_data[5:].split(',', 1)
-    # Will likely always have this kind of data type
-    if media != 'application/octet-stream;charset=utf-8':
-        raise ValueError(f'Cannot handle media type "{media}"')
-    csv_string = unquote_plus(data)
-    print(csv_string)
+		media, data = csv_data[5:].split(',', 1)
+		# Will likely always have this kind of data type
+		if media != 'application/octet-stream;charset=utf-8':
+			raise ValueError(f'Cannot handle media type "{media}"')
+		csv_string = unquote_plus(data)
+		
 
 	# Then leave the iframe
 	driver.switch_to.default_content()
-	#return 'Done downloading'
+	return csv_string
 
 
 def get_metadata(url):
@@ -74,19 +80,24 @@ def get_metadata(url):
 	
 	driver.quit() # does this close the tab?
 
-def get_case_series(csv_):
+def get_case_series(chart_id, url):
+	csv_ = extract_csvs(chart_id, url)
 	series = []
-	with open(csv_, mode = 'r') as case_csv: 
-		csv_reader = csv.DictReader(case_csv)
-		csv_headers = list(next(csv_reader).keys()) # TO-DO: Make it work without hard coding the keys
-		case_history = []
-		for row in csv_reader:
-			# TO-DO: throw an exception if there are more than the expected number of headers, or when order has changed
-			daily = {}
-			daily["date"] = row["Date"] # TO-DO: need to format the date properly
-			case_history.append(int(row["Total Cases"]))
-			daily["cumul_cases"] = row["Total Cases"]
-			series.append(daily)
+
+	csv_strs = csv_.splitlines()
+	keys = csv_strs[0].split(',')
+	#print(keys)
+	
+	case_history = []
+
+	# TO-DO: Double check this in morning, then transfer over logic to deaths
+	for row in csv_strs[1:]:
+	# 	# TO-DO: throw an exception if there are more than the expected number of headers, or when order has changed
+	 	daily = {}
+	 	daily["date"] = row.split(',')[0] # TO-DO: need to format the date properly
+	 	case_history.append(int(row.split(',')[1]))
+	 	daily["cumul_cases"] = row.split(',')[1]
+	 	series.append(daily)
 		
 	case_history_diff = np.diff(case_history) 
 	case_history_diff = np.insert(case_history_diff, 0, 0) # there will be no calculated difference for the first day, so adding it in manually
@@ -94,7 +105,8 @@ def get_case_series(csv_):
 		series[val]["cases"] = case_num
 	return series
 
-def get_death_series(csv_):
+def get_death_series(chart_id,  url):
+	csv_ = extract_csvs(chart_id, url)
 	series = []
 	with open(csv_, mode = 'r') as case_csv: 
 		csv_reader = csv.DictReader(case_csv)
