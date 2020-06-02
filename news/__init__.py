7082b36d05a757b2cc863c6e56e60666c6e10b1b
Rob Brackett <rob@robbrackett.com>
2020-06-01 19:28:45 -0700

    Add news scraper for Napa County, fixes #62

--- news/__init__.py
+++ news/__init__.py
@@ -3,7 +3,7 @@ from .alameda import AlamedaNews
 from .base import NewsScraper
 # from .contra_costa import ContraCostaNews
 # from .marin import MarinNews
-# from .napa import NapaNews
+from .napa import NapaNews
 from .san_francisco import SanFranciscoNews
 # from .san_mateo import SanMateoNews
 from .santa_clara import SantaClaraNews
@@ -15,7 +15,7 @@ scrapers: Dict[str, Type[NewsScraper]] = {
     'alameda': AlamedaNews,
     # 'contra_costa': ContraCostaNews,
     # 'marin': MarinNews,
-    # 'napa': NapaNews,
+    'napa': NapaNews,
     'san_francisco': SanFranciscoNews,
     # 'san_mateo': SanMateoNews,
     'santa_clara': SantaClaraNews,
