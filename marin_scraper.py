a27073f6ae3debfaed5b038121b0f36e42799cef
kwonangela7 <kwonangela7@berkeley.edu>
2020-06-09 21:29:04 -0700

    finalized series and test scraping methods with function annotations and raised exceptions

--- marin_scraper.py
+++ marin_scraper.py
@@ -2,50 +2,52 @@
 import csv
 import json
 import numpy as np
+from typing import List, Dict, Tuple
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from urllib.parse import unquote_plus
 from datetime import datetime
+import re
 
+#from .utils import get_data_model
 
-# do a final check 
-def get_county_data():
+def get_county_data() -> Dict:
 	"""Main method for populating county data"""
 
 	url = 'https://coronavirus.marinhhs.org/surveillance'
 	with open('/Users/angelakwon/Desktop/data-covid19-sfbayarea/data-model.json') as template:
-		# TO-DO: Need to change this to github location
 		model = json.load(template)
+	#model = get_data_model()
 
-	csvs = {"cases": "Eq6Es", "deaths": "Eq6Es", "tests": None, "age": "VOeBm", "gender": "FEciW", "race_eth": "aBeEd", "transmission": None}
-	# NOTE: they used to have a pos/neg test csv, but it seems to be gone now. 
-	# also, their graph doesn't show pos/neg.
+	chart_ids = {"cases": "Eq6Es", "deaths": "Eq6Es", "tests": '2Hgir', "age": "VOeBm", "gender": "FEciW", "race_eth": "aBeEd"} 
 	# population totals and transmission data missing.
 
-	#model['name'] = 
-	#model['update_time'] = 
+	model['name'] = "Alameda"
+	model['update_time'] = datetime.today().isoformat()
+	# No actual update time on their website? They update most charts daily (so the isoformat is only partially correct.)
 	model['source_url'] = url
-	#model['meta_from_source'] = 
-	# make sure to get the comments below the data
-	#model['meta_from_baypd']
-	#model["series"]["cases"] = get_case_series(csvs["cases"], url) 
-	#model["series"]["deaths"] =  get_death_series(csvs["deaths"], url)
-	#model["series"]["tests"] = get_test_series(case_csv)
-	#model["case_totals"]["age_group"], model["death_totals"]["age_group"] = get_breakdown_age(csvs["age"], url)
-	#model["case_totals"]["gender"], model["death_totals"]["gender"] = get_breakdown_gender(csvs["gender"], url)
-	model["case_totals"]["race_eth"], model["death_totals"]["race_eth"] = get_breakdown_race_eth(csvs["race_eth"], url)
-	print(model["case_totals"]["race_eth"], model["death_totals"]["race_eth"])
+	#model['meta_from_source'] = get_metadata(url, chart_ids)
+	model["series"]["cases"] = get_case_series(chart_ids["cases"], url) 
+	model["series"]["deaths"] =  get_death_series(chart_ids["deaths"], url)
+	model["series"]["tests"] = get_test_series(chart_ids["tests"], url)
+	model["case_totals"]["age_group"], model["death_totals"]["age_group"] = get_breakdown_age(chart_ids["age"], url)
+	model["case_totals"]["gender"], model["death_totals"]["gender"] = get_breakdown_gender(chart_ids["gender"], url)
+	model["case_totals"]["race_eth"], model["death_totals"]["race_eth"] = get_breakdown_race_eth(chart_ids["race_eth"], url)
 	
+	#model["series"]["tests"] = get_test_series(case_csv)
 
-def extract_csvs(chart_id, url):
-	driver = webdriver.Chrome('/Users/angelakwon/Downloads/chromedriver') # can I leave this blank, will virtual env take care of it?
+	print(model)
+
+def extract_csvs(chart_id: str, url: str) -> str:
+	"""This method extracts the csv string from the data wrapper charts."""
+	driver = webdriver.Chrome('/Users/angelakwon/Downloads/chromedriver') 
+	# need to figure out how to change the webdriver
 	
 	driver.implicitly_wait(30)
 	driver.get(url)
 
-	#frame = driver.find_element_by_css_selector(f'iframe[src^="//datawrapper.dwcdn.net/{chart_id}/"]')
-	# the link changed - now it attaches a random number after the chart id so I needed to change the attribute.
 	frame = driver.find_element_by_css_selector(f'iframe[src*="//datawrapper.dwcdn.net/{chart_id}/"]')
+
 	driver.switch_to.frame(frame)
 	# Grab the raw data out of the link's href attribute
 	csv_data = driver.find_element_by_class_name('dw-data-link').get_attribute('href')
@@ -59,92 +61,130 @@ def extract_csvs(chart_id, url):
 		if media != 'application/octet-stream;charset=utf-8':
 			raise ValueError(f'Cannot handle media type "{media}"')
 		csv_string = unquote_plus(data)
-		
 
 	# Then leave the iframe
 	driver.switch_to.default_content()
+
 	return csv_string
 
-
-def get_metadata(url):
-	# I think I want to return a list
-
+def get_metadata(url: str, chart_ids: str) -> Tuple:
 	notes = []
-	driver = webdriver.Chrome('/Users/angelakwon/Downloads/chromedriver')
+	driver = webdriver.Chrome('/Users/angelakwon/Downloads/chromedriver') # change this to point to Github one
 	driver.implicitly_wait(30)
 	driver.get(url)
 	soup = BeautifulSoup(driver.page_source, 'html5lib')
-	soup.find_all('p') .getText()
-	# THEN use the getText function
+	metadata = []
+
+	to_be_matched = ['Total Cases, Recovered, Hospitalizations and Deaths by Date Reported', 'Daily Count of Positive Results and Total Tests for Marin County Residents by Test Date ', 'Cases, Hospitalizations, and Deaths by Age, Gender and Race/Ethnicity ']
+	chart_metadata = []
+
+	for text in to_be_matched:
+		target = soup.find('h4',text=text)
+		if not target:
+			raise ValueError('Cannot handle this header.')
+		for sib in target.find_next_siblings()[:1]: # I only want the first paragraph tag
+			metadata += sib.text
+
+
+	# Metadata for each csv file I pull. There's probably a better way to organize this.
+	for chart_id in chart_ids.values():
+		frame = driver.find_element_by_css_selector(f'iframe[src*="//datawrapper.dwcdn.net/{chart_id}/"]')
+		driver.switch_to.frame(frame)
+		for c in driver.find_elements_by_class_name('dw-chart-notes'):
+			chart_metadata.append(soup.find('div').getText())
+
+		# Switch back to the parent frame to "reset" the context
+		driver.switch_to.parent_frame()
 	
 	driver.quit() 
+	# Return the metadata. I take the set of the chart_metadata since there are repeating metadata strings.
+	return metadata, list(set(chart_metadata)) 
 
-def get_case_series(chart_id, url):
+def get_case_series(chart_id: str, url: str) -> List:
+	"""This method extracts the date, number of cumulative cases, and new cases."""
 	csv_ = extract_csvs(chart_id, url)
 	series = []
 
 	csv_strs = csv_.splitlines()
 	keys = csv_strs[0].split(',')
-	#print(keys)
 	
+	if keys != ['Date', 'Total Cases', 'Total Recovered*', 'Total Hospitalized', 'Total Deaths']:
+		raise ValueError('The headers have changed')
+
 	case_history = []
 
 	for row in csv_strs[1:]:
-	# 	# TO-DO: throw an exception if there are more than the expected number of headers, or when order has changed
 	 	daily = {}
-	 	#daily["date"] = row.split(',')[0] 
+	 	# Grab the date in the first column
 	 	date_time_obj = datetime.strptime(row.split(',')[0], '%m/%d/%Y')
 	 	daily["date"] = date_time_obj.isoformat()
-	 	# TO-DO: need to format the date properly
+	 	# Collect the case totals in order to compute the change in cases per day 
 	 	case_history.append(int(row.split(',')[1]))
+	 	# Grab the cumulative number in the fifth column
 	 	daily["cumul_cases"] = int(row.split(',')[1])
 	 	series.append(daily)
 		
 	case_history_diff = np.diff(case_history) 
-	case_history_diff = np.insert(case_history_diff, 0, 0) # there will be no calculated difference for the first day, so adding it in manually
+	# there will be no calculated difference for the first day, so adding it in manually
+	case_history_diff = np.insert(case_history_diff, 0, 0) 
+	# adding the case differences into the series
 	for val, case_num in enumerate(case_history_diff):
 		series[val]["cases"] = case_num
 	return series
 
-def get_death_series(chart_id,  url):
+def get_death_series(chart_id: str, url: str) -> List:
+	"""This method extracts the date, number of cumulative deaths, and new deaths."""
 	csv_ = extract_csvs(chart_id, url)
 	series = []
 
 	csv_strs = csv_.splitlines()
 	keys = csv_strs[0].split(',')
-	
+	if keys != ['Date', 'Total Cases', 'Total Recovered*', 'Total Hospitalized', 'Total Deaths']:
+		raise ValueError('The headers have changed.')
+
 	death_history = []
 
 	for row in csv_strs[1:]:
-	# 	# TO-DO: throw an exception if there are more than the expected number of headers, or when order has changed
 	 	daily = {}
+	 	# Grab the date in the first column
 	 	date_time_obj = datetime.strptime(row.split(',')[0], '%m/%d/%Y')
 	 	daily["date"] = date_time_obj.isoformat()
+	 	# Collect the death totals in order to compute the change in deaths per day 
 	 	death_history.append(int(row.split(',')[4]))
+	 	# Grab the cumulative number in the fifth column
 	 	daily["cumul_deaths"] = int(row.split(',')[4])
 	 	series.append(daily)
 		
 	death_history_diff = np.diff(death_history) 
-	death_history_diff = np.insert(death_history_diff, 0, 0) # there will be no calculated difference for the first day, so adding it in manually
+	# there will be no calculated difference for the first day, so adding it in manually
+	death_history_diff = np.insert(death_history_diff, 0, 0) 
+	# adding the case differences into the series
 	for val, death_num in enumerate(death_history_diff):
 		series[val]["deaths"] = death_num
 	return series
 
-
-def get_breakdown_age(chart_id, url):
-	""" Gets breakdown of cases and deaths by age """
+def get_breakdown_age(chart_id: str, url: str) -> Tuple:
+	"""This method gets the breakdown of cases and deaths by age."""
 	csv_ = extract_csvs(chart_id, url)
 	c_brkdown = []
 	d_brkdown = []
 
 	csv_strs = csv_.splitlines()
-	keys = csv_strs[0].split(',') # don't know if this is actually needed
+	keys = csv_strs[0].split(',') 
 
-	
+	if keys != ['Age Category', 'POPULATION', 'Cases', 'Hospitalizations', 'Deaths']:
+		raise ValueError('The headers have changed')
+
+	ages = ['0-18', '19-34', '35-49', '50-64', '65'] 
 	for row in csv_strs[1:]:
 		c_age = {}
 		d_age = {}
+		# Extracting the age group and the raw count (the 3rd and 5th columns, respectively) for both cases and deaths.
+		# Each new row has data for a different age group.
 		c_age["group"] = row.split(',')[0]
+		print(type(c_age["group"]))
+		if c_age["group"] not in ages:
+			raise ValueError('The age groups have changed.')
 		c_age["raw_count"] = int(row.split(',')[2])
 		d_age["group"] = row.split(',')[0]
 		d_age["raw_count"] = int(row.split(',')[4])
@@ -153,30 +193,42 @@ def get_breakdown_age(chart_id, url):
 
 	return c_brkdown, d_brkdown
 
-def get_breakdown_gender(chart_id, url):
-	""" Gets breakdown of cases and deaths by gender """
+def get_breakdown_gender(chart_id: str, url: str) -> Tuple:
+	"""This method gets the breakdown of cases and deaths by gender."""
 	csv_ = extract_csvs(chart_id, url)
 
 	csv_strs = csv_.splitlines()
-	keys = csv_strs[0].split(',') # don't know if this is actually needed
+	keys = csv_strs[0].split(',') 
+	if keys != ['Gender', 'POPULATION', 'Cases', 'Hospitalizations', 'Deaths']:
+		raise ValueError('The headers have changed.')
 
+	genders = ['Male', 'Female']
 	c_gender = {}
 	d_gender = {}
 	
 	for row in csv_strs[1:]:
+		# Extracting the gender and the raw count (the 3rd and 5th columns, respectively) for both cases and deaths.
+		# Each new row has data for a different gender.
 		split = row.split(',')
 		gender = split[0].lower()
+		if gender not in genders:
+			return ValueError('The genders have changed.')
 		c_gender[gender] = int(split[2])
 		d_gender[gender] = int(split[4])			
-		# check to see what other scrapers have done with missing data model values
 
 	return c_gender, d_gender
 
+def get_breakdown_race_eth(chart_id: str, url: str) -> Tuple:
+	"""This method gets the breakdown of cases and deaths by race/ethnicity."""
 
-def get_breakdown_race_eth(chart_id, url):
 	csv_ = extract_csvs(chart_id, url)
 
 	csv_strs = csv_.splitlines()
+	keys = csv_strs[0].split(',') 
+	
+	if keys != ['Race/Ethnicity', 'COUNTY POPULATION', 'Case Count', 'Percent of Cases', 'Hospitalization Count', 'Percent of Hospitalizations', 'Death Count', 'Percent of Deaths']:
+		raise ValueError("The headers have changed.")
+
 	key_mapping = {"black/african american":"African_Amer", "hispanic/latino": "Latinx_or_Hispanic",
             "american indian/alaska native": "Native_Amer", "native hawaiian/pacific islander": "Pacific_Islander", "white": "White", "asian": "Asian", "multi or other race": "Multi or Other Race"}
             # "Multiple_Race", "Other" are not separate in this data set - they are one value under "Multi or Other Race"
@@ -188,23 +240,55 @@ def get_breakdown_race_eth(chart_id, url):
 		split = row.split(',')
 		race_eth = split[0].lower()
 		if race_eth not in key_mapping:
-			print("New race_eth group")
+			raise ValueError("The race_eth groups have changed.")
 		else:
 			c_race_eth[key_mapping[race_eth]] = int(split[2])
 			d_race_eth[key_mapping[race_eth]] = int(split[6])
-		# check to see what other scrapers have done with missing data model values
 
 	return c_race_eth, d_race_eth
 
-#def get_breakdown_transmission():
+def get_test_series(chart_id: str, url: str) -> Tuple:
+	"""This method gets the date, the number of positive and negative tests on that date, and the number of cumulative positive and negative tests."""
 
-#def get_death_totals_underlying():
+	csv_ = extract_csvs(chart_id, url)
+	series = []
 
+	csv_strs = csv_.splitlines()
+	keys = csv_strs[0].split(',')
+	
+	test_history = []
+	
+	# Grab the dates, which are in the header
+	for entry in csv_strs[:1][0].split(',')[1:]: 
+		# need to exclude very first item in the csv_strs[:1][0].split(',') list (which is the value 'Date')
+		daily = {}
+		date_time_obj = datetime.strptime(entry, '%m/%d/%Y')
+		daily["date"] = date_time_obj.isoformat()
+		series.append(daily)
 
-#def get_test_series():
+	
+	# Grab the positive test result numbers, which is in the second row. 
+	# [1:] is included to make sure that 'Positive Tests' is not captured.
+	p_entries = csv_strs[1:2][0].split(',')[1:]
 
+	# initialize values
+	cumul_pos = int(p_entries[0])
+	series[0]["positive"] = int(p_entries[0])
+	series[0]["cumul_pos"] = cumul_pos
+	index = 1
+
+	while index < len(series):
+		# dictionary gets weird
+		day = series[index]
+		curr = int(p_entries[index])
+		day["positive"] = int(curr)
+		cumul_pos += curr
+		day["cumul_pos"] = cumul_pos 
+		index += 1
+
+	# Grab the negative test result numbers, which is in the third row.
+	# "negative", "cumul_neg"
+	
+	return series   
 
 get_county_data()
-
-
-# figure out a way to run the scraper through the command line 
