329f92d203afb4ed1cfda7240afb37466c9330f4
Logan Cooper <lcooper@forio.com>
2020-06-16 17:59:06 -0700

    sonoma Correct conventions for sonoma

--- covid19_sfbayarea/data/__init__.py
+++ covid19_sfbayarea/data/__init__.py
@@ -1,16 +1,16 @@
 from typing import Dict, Any
 from . import alameda
 from . import san_francisco
-import data_scrapers.sonoma_county as sonoma_county
+from . import sonoma
 
 scrapers: Dict[str, Any] = {
     'alameda': alameda,
     # 'contra_costa': None,
     # 'marin': None,
     # 'napa': None,
-    'san_francisco': san_francisco
+    'san_francisco': san_francisco,
     # 'san_mateo': None,
     # 'santa_clara': None,
     # 'solano': None,
-    'sonoma': sonoma_county,
+    'sonoma': sonoma,
 }
