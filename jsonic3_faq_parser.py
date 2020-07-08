b5abf471d6e7bb30eb25dae55bb88f6653837909
OvaNova <mail@andrewsmithauthor.com>
2020-07-07 18:27:50 -0700

    Script to parse FAQ Content sheet into JSON
    Rob's review notes taken in. Only skipped moving the questions list ('questions_list = [] # Create a list to contain questions') outside the loop - hope I understood that right! - because it causes every Question object to show up in every Section object. Also still looking at DictReader . . . not yet certain this will have much advantage given the format of the spreadsheet and of the parsed json we want (a UX/front end discrepancy that might be a lot of bother to resolve outside the code . . . still looking at that, though, and super-open to ideas :-)

---
+++ jsonic3_faq_parser.py
@@ -0,0 +1,79 @@
+"""
+Grabs data from the 'FAQ Content' CSV and turns it into nice JSON: a main faq object containing an array of Section objects, each Section Object in turn holding an array of Question objects consisting of question, answer and related link/s, as so:
+    [
+        {
+          'Q': 'What are the symptoms of COVID-19?',
+          'A': 'Symptoms of COVID-19 include coughing and shortness of breath. Additionally, a person showing two or more of the following symptoms may have the virus: fever, repeated shaking with chills, muscle pain, headache, sore throat, new loss of taste or smell.',
+          'link': 'https://www.cdc.gov/coronavirus/2019-ncov/symptoms-testing/symptoms.html'
+        },
+    .
+    .
+    .
+    ]
+    etc
+In addition to an array of Question objects, each Section incorporates its title and a 'last updated' date value. For the Python intermediary stage (between CSV stream  and JSON) we will create a hierarchy of nested dictionaries and lists equivalent to the JSON objects and arrays.
+"""
+
+import requests
+import json
+import csv
+import datetime
+
+
+# function to grab data from a google sheet + return as reader object
+def google_sheet_csv_data(sheet, gid):
+    url = f'https://docs.google.com/spreadsheets/d/{sheet}/export'
+    response = requests.get(url, params={
+        'format': 'csv',
+        'id': sheet,
+        'gid': gid
+    })
+    response.raise_for_status()
+    return csv.reader(response.iter_lines(decode_unicode=True))
+
+
+# Pass faq Content sheet id and gid to function
+reader = google_sheet_csv_data('1_wBXS62S5oBQrwetGc8_-dFvDjEmNqzqHwUeP-DzkYs',
+ '1318925039')
+
+# Date, to be updated each time the program runs, in UTC
+date = datetime.datetime.utcnow().strftime('%Y-%m-%d')
+
+# Create the main FAQ dictionary
+# Create a list to contain our Section objects
+# Place the Sections list inside the main FAQ dictionary
+faq_dict = {}
+sections_list = []
+faq_dict['faqItems'] = sections_list
+
+# Ensure only first two columns in each row are used
+# Now arrange the Sections and Questions in a JSON-friendly way
+for index, row in enumerate(reader):
+    rowtype, rowval, *_rest = row
+    if rowtype == 'Category':   # We don't need this row
+        pass
+    elif rowtype == 'Section Head':
+        section = {}   # Create a dictionary for a Section
+        sections_list.append(section)   # Add new Section to Sections list
+        section['title'] = rowval   # Give the Section its title
+        section['lastUpdatedAt'] = date   # a 'last updated' value
+        questions_list = []   # Create list to contain questions
+        section['qa'] = questions_list   # add our Questions list to its Section
+    elif rowtype == 'Q':
+        question = {}   # Create new Question dictionary
+        question['q'] = rowval   # the Question's title
+        questions_list.append(question)   # Append Question to its given Section
+    elif rowtype == 'A':   # Now the same for Answers & Links
+        question['a'] = rowval
+    elif rowtype == 'link':
+        question['url'] = rowval
+    elif rowtype == '':   # Some safeguards
+        pass
+    elif rowval and not rowtype:
+        raise ValueError(f'row {index} has a value but no description!')
+    else:
+        raise ValueError(f'Unknown row header: "{rowtype}"')
+
+# Create formatted json file
+with open('faq.json', 'w') as f:   # Create JSON output file
+    json.dump(faq_dict, f, indent=2)   # Put FAQ Content in output file as JSON
