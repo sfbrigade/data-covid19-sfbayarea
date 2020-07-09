cae0bfe05a1a02ee4bc0e4284483badcb834a64f
Ben Hancock <ben@benghancock.com>
2020-07-08 21:39:27 -0700

    handle API pagination

--- covid19_sfbayarea/data/hospitals.py
+++ covid19_sfbayarea/data/hospitals.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+import logging
 import requests
 from typing import Dict, List
 
@@ -14,22 +15,48 @@ from typing import Dict, List
 
 # URLs and APIs
 HOSPITALS_LANDING_PAGE = "https://data.ca.gov/dataset/covid-19-hospital-data#"
-CAGOV_API_BASEURL = "https://data.ca.gov/api/3/action/datastore_search"
+CAGOV_BASEURL = "https://data.ca.gov"
+CAGOV_API = "/api/3/action/datastore_search"
 HOSPITALS_RESOURCE_ID = "42d33765-20fd-44b8-a978-b083b7542225"
 RESULTS_LIMIT = 50
+
+logging.basicConfig(level=logging.INFO)
 
 
 def get_timeseries() -> List:
     """Fetch all pages of timeseries data from API endpoint"""
     timeseries = []
+    results_counter = 0
 
     params = {"resource_id": HOSPITALS_RESOURCE_ID, "limit": RESULTS_LIMIT}
-    r = requests.get(CAGOV_API_BASEURL, params=params).json()
+    url = CAGOV_BASEURL + CAGOV_API
 
-    # TODO get notes for each field
+    # Handle the pagination
+    while True:
+        if params:
+            r = requests.get(url, params=params).json()
+        else:
+            r = requests.get(url).json()
 
-    records = r["result"].get("records")
-    timeseries.extend(records)
+        # TODO get notes for each field
+        results = r.get("result")
+        total = int(results.get("total"))
+        records = results.get("records")
+        timeseries.extend(records)
+
+        results_counter = len(timeseries)
+        logging.info(f"Got {results_counter} results out of {total} ...")
+        more = results.get("_links").get("next")
+
+        # Don't ask for more pages than are ther
+        if more and results_counter <= total:
+            url = CAGOV_BASEURL + more
+            params = None
+
+        else:
+            break
+
+    logging.info("Collected all pages")
 
     return timeseries
 
