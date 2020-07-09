674d59166787c2024aa9d626b4f36e309cf6910d
Ben Hancock <ben@benghancock.com>
2020-07-08 20:50:40 -0700

    get first page of results

--- covid19_sfbayarea/data/hospitals.py
+++ covid19_sfbayarea/data/hospitals.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import json
 import requests
+from typing import Dict, List
 
 # This module fetches COVID-19 hospital data from the CA.gov open data portal.
 # The input data is fetched from an API endpoint, and appears to be updated at
@@ -17,6 +19,21 @@ HOSPITALS_RESOURCE_ID = "42d33765-20fd-44b8-a978-b083b7542225"
 RESULTS_LIMIT = 50
 
 
-params = {"resource_id": HOSPITALS_RESOURCE_ID, "limit": RESULTS_LIMIT}
-r = requests.get(CAGOV_API_BASEURL, params=params)
-print(r.text)
+def get_timeseries() -> List:
+    """Fetch all pages of timeseries data from API endpoint"""
+    timeseries = []
+
+    params = {"resource_id": HOSPITALS_RESOURCE_ID, "limit": RESULTS_LIMIT}
+    r = requests.get(CAGOV_API_BASEURL, params=params).json()
+
+    # TODO get notes for each field
+
+    records = r["result"].get("records")
+    timeseries.extend(records)
+
+    return timeseries
+
+
+if __name__ == "__main__":
+    """ When run as a script, prints the data to stdout"""
+    print(json.dumps(get_timeseries(), indent=4))
