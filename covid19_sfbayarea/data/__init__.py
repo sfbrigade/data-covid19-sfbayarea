5f2c3d1df68e7d7fa6f817bee6b8d3a605429a1f
Ricardo Piro-Rael <ricardo@hint.com>
2020-08-12 23:21:09 -0700

    Meta fetching working

--- covid19_sfbayarea/data/__init__.py
+++ covid19_sfbayarea/data/__init__.py
@@ -6,11 +6,11 @@ from . import solano
 from . import san_mateo
 
 scrapers: Dict[str, Any] = {
-    'alameda': alameda,
+    # 'alameda': alameda,
     # 'contra_costa': None,
     # 'marin': None,
     # 'napa': None,
-    'san_francisco': san_francisco,
+    # 'san_francisco': san_francisco,
     'san_mateo': san_mateo,
     # 'santa_clara': None,
     'sonoma': sonoma,
--- covid19_sfbayarea/data/san_mateo/meta.py
+++ covid19_sfbayarea/data/san_mateo/meta.py
@@ -1,34 +1,32 @@
 import json
+from functools import reduce
 from typing import Any, Dict, List
 from .power_bi_querier import PowerBiQuerier
 from requests import get
 
-class Meta(PowerBiQuerier):
-    def get_data(self) -> None:
-        fetched_meta = self._fetch_meta()
-
-    def _fetch_meta(self) -> Dict[Any]:
-        url = ''.join(
+class Meta():
+    def get_data(self) -> str:
+        url = ''.join([
             'https://wabi-us-gov-iowa-api.analysis.usgovcloudapi.net/public/reports/',
-            self.DEFAULT_POWERBI_RESOURCE_KEY,
+            PowerBiQuerier.DEFAULT_POWERBI_RESOURCE_KEY,
             '/modelsAndExploration?preferReadOnlySession=true'
-        )
-        response = get(url, headers = { 'X-PowerBI-ResourceKey': self.DEFAULT_POWERBI_RESOURCE_KEY })
-        return response.json()
+        ])
+        response = get(url, headers = { 'X-PowerBI-ResourceKey': PowerBiQuerier.DEFAULT_POWERBI_RESOURCE_KEY })
+        return self._extract_meta(response.json())
 
-    def _extract_meta(self, response_json: Dict[Any]) -> None:
+    def _extract_meta(self, response_json: Dict[str, Any]) -> str:
         visual_containers = self._dig(response_json, ['exploration', 'sections', 0, 'visualContainers'])
-        visual_container_with_meta = _find_visual_container_with_meta(visual_containers)
-        serialized_config = visual_containers[8]['config']
-        config = json.loads(serialized_config)
-        return config['singleVisual']['objects']['general'][0]['properties']['paragraphs'][0]['textRuns'][0]['value']
+        text_boxes = self._extract_text_runs(visual_containers)
+        return max(text_boxes, key=len)
 
-    def _find_meta_from_visual_containers(self, visual_containers: List[Dict]) -> str:
-        configs = [json.loads(visual_container['config']) for visual_container in visual_containers]
-        objects = [config['singleVisual']['objects'] for config in configs]
-        objects_with_general_key = [object for object in objects if 'general' in object.keys()]
-        properties = [object['properties'] for objects in objects_with_general_key for object in objects['general']]
-        filter()
+    def _extract_text_runs(self, containers: List[Any]) -> List[str]:
+        configs_with_text = [json.loads(container['config']) for container in containers if 'textRuns' in container['config']]
+        paragraphs = self._extract_paragraphs(configs_with_text)
+        return [text_run['value'] for paragraph in paragraphs for text_run in paragraph['textRuns']]
+
+    def _extract_paragraphs(self, configs: List[List[Dict]]):
+        json_path = ['singleVisual', 'objects', 'general', 0, 'properties', 'paragraphs']
+        return [paragraph for config in configs for paragraph in self._dig(config, json_path)]
 
     def _dig(self, results: Dict[str, List], json_path: List[Any]) -> List[Dict[str, int]]:
         try:
