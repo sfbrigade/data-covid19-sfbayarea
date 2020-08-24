6ec69c2a23b1eeefc99c8912036f6abded7bf55c
kengoy <Kengo.Yoshii.925@gmail.com>
2020-08-23 22:51:33 -0700

    Changed US to United States due to CDS data source change.

--- scraper.py
+++ scraper.py
@@ -52,15 +52,15 @@ def pipeline(counties: List[str]) -> Dict[str, Dict]:
     return county_data
 
 bay_area_counties = [
-    'Solano County, California, US',
-    'Alameda County, California, US',
-    'Santa Clara County, California, US',
-    'San Francisco County, California, US',
-    'Contra Costa County, California, US',
-    'San Mateo County, California, US',
-    'Sonoma County, California, US',
-    'Napa County, California, US',
-    'Marin County, California, US'
+    'Solano County, California, United States',
+    'Alameda County, California, United States',
+    'Santa Clara County, California, United States',
+    'San Francisco County, California, United States',
+    'Contra Costa County, California, United States',
+    'San Mateo County, California, United States',
+    'Sonoma County, California, United States',
+    'Napa County, California, United States',
+    'Marin County, California, United States'
 ]
 covid_data = pipeline(bay_area_counties)
 print(json.dumps(covid_data, indent=4))
