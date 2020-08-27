2dc626804ffba8869e25cf64fe5a65d6c69689de
Ricardo Piro-Rael <ricardo@hint.com>
2020-08-26 20:38:41 -0700

    Do not map ethnicity to data model

--- covid19_sfbayarea/data/san_mateo/cases_by_ethnicity.py
+++ covid19_sfbayarea/data/san_mateo/cases_by_ethnicity.py
@@ -1,6 +1,5 @@
 from typing import Dict, List
 from .power_bi_querier import PowerBiQuerier
-from .utils import map_ethnicity_to_data_model
 
 class CasesByEthnicity(PowerBiQuerier):
     def __init__(self) -> None:
@@ -11,4 +10,4 @@ class CasesByEthnicity(PowerBiQuerier):
 
     def _parse_data(self, response_json: Dict[str, List]) -> Dict[str, int]: # type: ignore
         results = super()._parse_data(response_json)
-        return { map_ethnicity_to_data_model(ethnicity): count for ethnicity, count in results }
+        return { ethnicity.strip(): count for ethnicity, count in results }
--- covid19_sfbayarea/data/san_mateo/utils.py
+++ covid19_sfbayarea/data/san_mateo/utils.py
@@ -1,21 +1,6 @@
 from functools import reduce
 from typing import Any, Dict, List
 
-def map_ethnicity_to_data_model(ethnicity: str) -> str:
-    ethnicity_without_whitespace = ethnicity.strip()
-    mapping = {
-        'American Indian/Alaska Native': 'Native_Amer',
-        'Asian': 'Asian',
-        'Latino/Hispanic': 'Latinx_or_Hispanic',
-        'Black': 'African_Amer',
-        'Multirace': 'Multiple_Race',
-        'Other': 'Other',
-        'Pacific Islander': 'Pacific_Islander',
-        'White': 'White',
-        'Unknown': 'Unknown'
-    }
-    return mapping.get(ethnicity_without_whitespace, ethnicity_without_whitespace)
-
 def dig(items: Dict[str, Any], json_path: List[Any]) -> Any:
     try:
         return reduce(lambda subitem, next_step: subitem[next_step], json_path, items)
