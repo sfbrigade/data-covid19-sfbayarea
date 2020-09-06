c3e344abbbe1447b1b92bd63e77e961a198fb5a0
Ben Hancock <ben@benghancock.com>
2020-09-05 20:28:26 -0700

    address spacing and style issues picked up by linter

--- scraper_data.py
+++ scraper_data.py
@@ -7,7 +7,7 @@ from typing import Tuple
 from pathlib import Path
 
 
-COUNTY_NAMES : Tuple[str,...]= tuple(data_scrapers.scrapers.keys())
+COUNTY_NAMES: Tuple[str, ...] = tuple(data_scrapers.scrapers.keys())
 
 
 @click.command(help='Create a .json with data for one or more counties. Supported '
@@ -16,7 +16,7 @@ COUNTY_NAMES : Tuple[str,...]= tuple(data_scrapers.scrapers.keys())
                 type=click.Choice(COUNTY_NAMES, case_sensitive=False))
 @click.option('--output', metavar='PATH',
               help='write output file to this directory')
-def main(counties: Tuple[str,...], output: str) -> None:
+def main(counties: Tuple[str, ...], output: str) -> None:
     out = dict()
     failed_counties = False
     if len(counties) == 0:
@@ -28,19 +28,23 @@ def main(counties: Tuple[str,...], output: str) -> None:
             out[county] = data_scrapers.scrapers[county].get_county()
         except Exception as e:
             failed_counties = True
-            print(f'{county} failed to scrape: {e}', file = stderr)
+            print(f'{county} failed to scrape: {e}', file=stderr)
 
     if output:
         parent = Path(output)
-        parent.mkdir(exist_ok = True) # if output directory does not exist, create it
+        parent.mkdir(exist_ok=True)   # if output directory does not exist, create it
         with parent.joinpath('data.json').open('w', encoding='utf-8') as f:
             json.dump(out, f, ensure_ascii=False, indent=2)
 
     else:
-        print(json.dumps(out,indent=2))
+        print(json.dumps(out, indent=2))
 
-    if not out: exit(70) # all counties failed
-    if failed_counties: exit(1) # some counties failed
+    if not out:
+        exit(70)  # all counties failed
+
+    if failed_counties:
+        exit(1)   # some counties failed
+
 
 if __name__ == '__main__':
     main()
