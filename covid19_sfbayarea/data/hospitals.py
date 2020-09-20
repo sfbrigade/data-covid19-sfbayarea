381a293e4efc90a628b287c3d6f7f9666584ebe8
Ben Hancock <ben@benghancock.com>
2020-09-20 13:50:33 -0700

    reorganize hospitals.py and start wiring up hospital data scraper

--- covid19_sfbayarea/data/hospitals.py
+++ covid19_sfbayarea/data/hospitals.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-import json
 import logging
 import requests
 from datetime import datetime
 from dateutil import tz
 from dateutil.parser import parse
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Union, Tuple
+
+from covid19_sfbayarea.utils import friendly_county
 
 # This module fetches COVID-19 hospital data from the CA.gov open data portal.
 # The input data is fetched from an API endpoint, and appears to be updated at
@@ -20,126 +21,22 @@ HOSPITALS_LANDING_PAGE = "https://data.ca.gov/dataset/covid-19-hospital-data#"
 CAGOV_BASEURL = "https://data.ca.gov"
 CAGOV_API = "/api/3/action/datastore_search"
 HOSPITALS_RESOURCE_ID = "42d33765-20fd-44b8-a978-b083b7542225"
-RESULTS_LIMIT = 50
+RESULTS_LIMIT = 200
 
 # For the output data
-SERIES_NAME = "Hospitalization"
+SERIES_NAME = "CA COVID-19 Hospitalization Data"
 BAYPD_META = "This data was pulled from the data.ca.gov CKAN Data API"
 
 logging.basicConfig(level=logging.INFO)
 
 
-def get_county(county: str) -> Dict:
-    """Return data just for the selected county. Include field notes.
-    This is basically an alias for `get_timeseries()` provided for consistency.
-    """
-    data = get_timeseries(county)
-
-    return data
-
-
-def get_timeseries(county: str = "all") -> Dict:
-    """Fetch all pages of timeseries data from API endpoint"""
-    ts_data: Dict[str, Union[str, List]] = {}
-    timeseries: List[Dict[str, Any]] = []
-    series_standardized: List[Dict[str, Any]] = []
-
-    # Add header data
-    if county == "all":
-        ts_data["name"] = f"{SERIES_NAME} - All CA Counties"
-    else:
-        ts_data["name"] = f"{SERIES_NAME} - {county.title()} County"
-
-    now = datetime.now(tz.tzutc()).isoformat(timespec="minutes")
-    ts_data["update_time"] = now
-    ts_data["source_url"] = HOSPITALS_LANDING_PAGE
-    ts_data["meta_from_baypd"] = BAYPD_META
-
-    # Call may be made without params on subsequent calls
-    params: Dict[str, Union[int, str]] = {
-        "resource_id": HOSPITALS_RESOURCE_ID,
-        "limit": RESULTS_LIMIT
-    }
-
-    if county != "all":
-        params["q"] = county.title()
-
-    url = CAGOV_BASEURL + CAGOV_API
-
-    try:
-        # Handle the pagination
-        while True:
-            # pass params if we don't have timeseries data yet
-            if not timeseries:
-                r = requests.get(url, params=params)
-
-            else:
-                r = requests.get(url)
-
-            r.raise_for_status()
-            results = r.json().get("result")
-            total = int(results.get("total"))
-
-            # Get notes only on the first pull
-            if not ts_data.get("meta_from_source"):
-                notes = results.get("fields")
-                ts_data["meta_from_source"] = notes
-
-            else:
-                pass
-
-            records = results.get("records")
-            timeseries.extend(records)
-
-            results_count = len(timeseries)
-            logging.info(f"Got {results_count} results out of {total} ...")
-            more = results.get("_links").get("next")
-
-            # Don't ask for more pages than there are
-            if more and results_count < total:
-                url = CAGOV_BASEURL + more
-
-            else:
-                break
-
-        # standardize the format of the data in a new list
-        for record in timeseries:
-            series_standardized.append(standardize_data(record))
-
-        ts_data["series"] = series_standardized
-        logging.info("Collected all pages")
-
-    except AttributeError:
-        logging.exception("Error parsing response")
-
-    except requests.exceptions.RequestException:
-        logging.exception("Error fetching from API")
-
-    finally:
-        return ts_data
-
-
-def standardize_data(record: Dict) -> Dict:
-    """Transform certain data fields to make them conform to BAPD style
-
-    Specifically:
-    - truncate timestamps to ISO 8601-formatted dates
-    - remove "rank" field, if it exists
-    - convert all 'null' values to -1
-    - cast zero-point floats as int
-
-    Also, the key 'todays_date' is converted to 'report_date' for clarity.
-    """
-    record["report_date"] = truncate_ts(record.pop("todays_date"))
-    record.pop("rank", None)
-    record = convert_null(record)
-    record = floats_to_ints(record)
-
-    return record
+# =======================
+# Data Manipulation Utils
+# =======================
 
 
 def truncate_ts(ts: str) -> str:
-    """Truncate a timestampe to an ISO 8601-formatted date"""
+    """Truncate a timestamp to an ISO 8601-formatted date"""
     trunc_ts = parse(ts).date().isoformat()
     return trunc_ts
 
@@ -181,6 +78,119 @@ def floats_to_ints(record: Dict) -> Dict:
     return record
 
 
-if __name__ == "__main__":
-    """When run as a script, prints all data to stdout"""
-    print(json.dumps(get_timeseries(), indent=4))
+# ===================
+# Data Transformation
+# ===================
+
+
+def standardize_data(record: Dict) -> Dict:
+    """Transform certain data fields to make them conform to BAPD style
+
+    Specifically:
+    - truncate timestamps to ISO 8601-formatted dates
+    - remove "rank" field, if it exists
+    - convert all 'null' values to -1
+    - cast zero-point floats as int
+
+    Also, the key 'todays_date' is converted to 'report_date' for clarity.
+    """
+    record["report_date"] = truncate_ts(record.pop("todays_date"))
+    record.pop("rank", None)
+    record = convert_null(record)
+    record = floats_to_ints(record)
+
+    return record
+
+
+def process_data(series: List, counties: Tuple) -> Dict:
+    """Transform the timeseries data (a list of dicts) into a dict
+    with keys for each county name, and a list of dicts with records
+    for that particular county"""
+    processed_series: Dict[str, Union[str, Any]] = {}
+
+    for county in counties:
+        county_name = friendly_county(county)
+        county_records = [
+            standardize_data(record) for record in series
+            if record.get("county") == county_name
+        ]
+
+        processed_series[county_name] = county_records
+
+    return processed_series
+
+
+# ====================
+# Data API Interaction
+# ====================
+
+
+def get_timeseries(counties: Tuple) -> Dict:
+    """Fetch all pages of timeseries data from API endpoint"""
+    ts_data: Dict[Any, Any] = {}
+    timeseries_raw: List[Dict[str, Any]] = []
+
+    now = datetime.now(tz.tzutc()).isoformat(timespec="minutes")
+
+    # Add header data
+    ts_data["name"] = SERIES_NAME
+    ts_data["update_time"] = now
+    ts_data["source_url"] = HOSPITALS_LANDING_PAGE
+    ts_data["meta_from_baypd"] = BAYPD_META
+
+    # Call may be made without params on subsequent calls
+    params: Dict[str, Union[int, str]] = {
+        "resource_id": HOSPITALS_RESOURCE_ID,
+        "limit": RESULTS_LIMIT
+    }
+
+    url = CAGOV_BASEURL + CAGOV_API
+
+    try:
+        # Handle the pagination
+        while True:
+            # pass params if we don't have timeseries data yet
+            if not timeseries_raw:
+                r = requests.get(url, params=params)
+
+            else:
+                r = requests.get(url)
+
+            r.raise_for_status()
+            results = r.json().get("result")
+            total = int(results.get("total"))
+
+            # Get notes only on the first pull
+            if not ts_data.get("meta_from_source"):
+                notes = results.get("fields")
+                ts_data["meta_from_source"] = notes
+
+            else:
+                pass
+
+            records = results.get("records")
+            timeseries_raw.extend(records)
+
+            results_count = len(timeseries_raw)
+            logging.info(f"Got {results_count} results out of {total} ...")
+            more = results.get("_links").get("next")
+
+            # Don't ask for more pages than there are
+            if more and results_count < total:
+                url = CAGOV_BASEURL + more
+
+            else:
+                break
+        logging.info("Collected all pages")
+
+        # standardize the format of the data and key it by county name
+        ts_data["series"] = process_data(timeseries_raw, counties)
+
+    except AttributeError:
+        logging.exception("Error parsing response")
+
+    except requests.exceptions.RequestException:
+        logging.exception("Error fetching from API")
+
+    finally:
+        return ts_data
---
+++ scraper_hospital_data.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+"""Script for pulling down CA COVID-19 hospitalization stats"""
+
+import click
+import json
+from typing import Tuple
+
+from covid19_sfbayarea.data import hospitals
+from covid19_sfbayarea.utils import cli_friendly_county
+
+
+# prep lists of counties the user can choose to filter the data
+# format county strings so they're command line-friendly
+with open("covid19_sfbayarea/counties.json") as f:
+    ca_county_list = json.load(f)
+
+bay_area_counties = [
+    cli_friendly_county(county) for county in ca_county_list.get("Bay Area")
+]
+
+other_ca_counties = [
+    cli_friendly_county(county) for county in ca_county_list.get("Other CA")
+]
+
+all_ca_counties = bay_area_counties + other_ca_counties
+
+
+@click.command(
+    help="Pull down COVID-19-related hospitalization data "
+         "from the California Dept. of Public Health"
+)
+@click.argument(
+    "counties",
+    metavar='[COUNTY]...',
+    nargs=-1,
+    type=click.Choice(all_ca_counties, case_sensitive=False)
+)
+@click.option(
+    '--output',
+    metavar='PATH',
+    help='write output file to this directory'
+)
+def main(counties: Tuple[str, ...], output: str) -> None:
+    if counties:
+        out = hospitals.get_timeseries(counties)
+
+    else:
+        out = hospitals.get_timeseries(bay_area_counties)
+
+    print(json.dumps(out, indent=2))
+
+
+if __name__ == '__main__':
+    main()
