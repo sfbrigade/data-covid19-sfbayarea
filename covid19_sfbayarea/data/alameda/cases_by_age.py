9238acd85d4685ea1d1c7deb61e2590c4a5f7a1d
Ricardo Piro-Rael <ricardo@hint.com>
2020-09-30 23:16:01 -0700

    Cases by age and gender working for Alameda

--- covid19_sfbayarea/data/alameda/cases_by_age.py
+++ covid19_sfbayarea/data/alameda/cases_by_age.py
@@ -1,13 +1,26 @@
-from typing import Dict, List
+from typing import Any, Dict, List
 from .power_bi_querier import PowerBiQuerier
 
 class CasesByAge(PowerBiQuerier):
     def __init__(self) -> None:
-        self.source = 'c1'
-        self.name = 'cases_by_age'
-        self.property = 'age_cat'
+        self.source = 'v'
+        self.name = 'V_Combined_data'
+        self.property = 'AgeGroup'
         super().__init__()
 
     def _parse_data(self, response_json: Dict[str, List]) -> List[Dict[str, int]]:
         results = super()._parse_data(response_json)
-        return [ { 'group': group, 'count': count } for group, count in results ]
+        return [ { 'group': group, 'count': count } for group, count in results if group != 'Unknown Age' ]
+
+    def _select(self) -> List[Dict[str, Any]]:
+        property = 'NumberOfCases'
+        return [
+            {
+                'Column': self._column_expression(self.property),
+                'Name': f'{self.name}.{self.property}'
+            },
+            {
+                'Measure': self._column_expression(property),
+                'Name': f'{self.name}.{property}'
+            },
+       ]
--- covid19_sfbayarea/data/alameda/cases_by_gender.py
+++ covid19_sfbayarea/data/alameda/cases_by_gender.py
@@ -1,13 +1,27 @@
-from typing import Dict, List
+from typing import Any, Dict, List
 from .power_bi_querier import PowerBiQuerier
 
 class CasesByGender(PowerBiQuerier):
     def __init__(self) -> None:
-        self.source = 'c1'
-        self.name = 'cases_by_sex'
-        self.property = 'sex'
+        self.source = 'v'
+        self.name = 'V_Combined_data'
+        self.property = 'Gender'
         super().__init__()
 
     def _parse_data(self, response_json: Dict[str, List]) -> List[Dict[str, int]]:
         results = super()._parse_data(response_json)
-        return [ { gender.lower(): count } for gender, count in results ]
+        data_pairs = filter(lambda result: len(result) == 2, results)
+        return [ { gender.lower(): count } for gender, count in data_pairs ]
+
+    def _select(self) -> List[Dict[str, Any]]:
+        property = 'NumberOfCases'
+        return [
+            {
+                'Column': self._column_expression(self.property),
+                'Name': f'{self.name}.{self.property}'
+            },
+            {
+                'Measure': self._column_expression(property),
+                'Name': f'{self.name}.{property}'
+            },
+       ]
