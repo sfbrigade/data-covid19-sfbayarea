8f92b7505b784ebc3743e901a4adcb0613f7de13
Rob Brackett <rob@robbrackett.com>
2020-10-09 11:37:06 -0700

    HOTFIX: handle messy links in Alameda news titles
    The Alameda news page now has links that take up only part of the title. The parser sometimes identifies these as links to language-specific versions of the news item, which causes it to stop parsing the title and start looking for language links. However, these links aren't language links, and we need to continue parsing the title as we move through them.
    
    This fixes the issue by being *much* more specific about what we expect for the text of a language link. This may have issues if there are ever typos in the language names, but seems to work well enough.
    
    Fixes #140.

---
+++ tests/news/alameda_test.py
@@ -0,0 +1,66 @@
+from covid19_sfbayarea.news import scrapers
+from covid19_sfbayarea.news.alameda import AlamedaNews
+from covid19_sfbayarea.news.feed import NewsItem
+from covid19_sfbayarea.news.utils import PACIFIC_TIME
+from datetime import datetime
+import os
+import pytest
+from typing import List
+from unittest.mock import patch
+
+
+# Determine whether to run tests that make actual live web requests.
+# See `tests/data/test_valid_output.py` for more explanation.
+# FIXME: create a pytest fixture that centralizes this code and the original
+# version in `tests/data/test_valid_output.py`.
+LIVE_TESTS = os.getenv('LIVE_TESTS', '').lower().strip()
+TEST_COUNTIES: List[str] = []
+if LIVE_TESTS in ('1', 't', 'true', '*', 'all'):
+    TEST_COUNTIES = list(scrapers.keys())
+elif LIVE_TESTS:
+    TEST_COUNTIES = [county
+                     for county in (county.strip()
+                                    for county in LIVE_TESTS.split(','))
+                     if county]
+
+
+@pytest.mark.skipif('alameda' not in TEST_COUNTIES, reason='Live testing not enabled for Alameda county')
+def test_alameda_news() -> None:
+    """Basic test of whether Alameda news works without obvious errors."""
+    feed = AlamedaNews.get_news()
+    assert len(feed.items) > 1
+    for item in feed.items:
+        assert isinstance(item.id, str)
+        assert '' != item.id
+        assert isinstance(item.title, str)
+        assert '' != item.title
+        assert isinstance(item.date_published, datetime)
+        assert item.date_published.tzinfo is not None
+
+
+def test_parses_titles_with_single_word_links() -> None:
+    # This is a stripped-down version of the Alameda page's current markup,
+    # which may change.
+    mock_html = """<!DOCTYPE html>
+        <html>
+            <body>
+                <div id="mainCol">
+                    <strong>September 28, 2020</strong>
+                    Guidance on How to Celebrate Halloween and D<a title="Joint Statement 9/28/2020" href="/covid19-assets/docs/press/joint-statement-2020.09.28.pdf" target="_blank">&iacute;</a>a de los Muertos Safely
+                    <a title="Joint Statement 9/28/2020" href="/covid19-assets/docs/press/joint-statement-2020.09.28.pdf" target="_blank">English</a>
+                    |
+                    <a title="Joint Statement re: Halloween &amp; Dia de los Muertos" href="/covid19-assets/docs/press/joint-health-officer-statement-about-reopening-spa-2020.09.28.pdf" target="_blank">Spanish</a>
+                </div>
+            </body>
+        </html>
+    """
+    with patch.object(AlamedaNews, 'load_html', return_value=mock_html):
+        feed = AlamedaNews.get_news()
+        assert 1 == len(feed.items)
+        assert NewsItem(
+            id='https://covid-19.acgov.org/covid19-assets/docs/press/joint-statement-2020.09.28.pdf',
+            url='https://covid-19.acgov.org/covid19-assets/docs/press/joint-statement-2020.09.28.pdf',
+            title='Guidance on How to Celebrate Halloween and Día de los Muertos Safely',
+            date_published=datetime(2020, 9, 28, tzinfo=PACIFIC_TIME),
+            summary=''
+        ) == feed.items[0]
