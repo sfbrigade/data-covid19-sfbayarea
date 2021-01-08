6c4585be592f14ed49a85df1557d4568e79188de
Rob Brackett <rob@robbrackett.com>
2021-01-07 19:23:19 -0800

    Add data scraper for Santa Clara (#164)
    At some point between the original issue and now, Santa Clara added all the data behind their dashboard to a Socrata data portal, making it much easier to load based on @elaguerta’s earlier work.
    
    Some items to note here:
    - There’s no demographic data for tests. (Or at least I couldn’t find it.)
    - There’s a new transmission category here we haven’t had in other counties: `outbreak_associated`. This means the case was related to an outbreak at a particular location (e.g. a factory) or event. More details in the "meta".
    - Santa Clara does not differentiate community spread and unknown transmission, so these both get categorized as `unknown` and `community` gets a value of `-1`.
    
    Fixes #28.

--- covid19_sfbayarea/data/__init__.py
+++ covid19_sfbayarea/data/__init__.py
@@ -5,6 +5,7 @@ from . import marin
 from . import sonoma
 from . import solano
 from . import san_mateo
+from . import santa_clara
 
 scrapers: Dict[str, Any] = {
     'alameda': alameda,
@@ -13,7 +14,7 @@ scrapers: Dict[str, Any] = {
     # 'napa': None,
     'san_francisco': san_francisco,
     'san_mateo': san_mateo,
-    # 'santa_clara': None,
+    'santa_clara': santa_clara,
     'sonoma': sonoma,
     'solano': solano,
 }
