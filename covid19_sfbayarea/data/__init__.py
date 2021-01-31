f84eb47a907c8c64f9556229be90c2db1d4beb8f
Rob Brackett <rob@robbrackett.com>
2021-01-31 00:51:11 -0800

    Add Napa County data scraper
    This adds data support for Napa County (wow, that was a long time coming). It's mostly based on the ArcGIS feature server, which lists *every case* in Napa County!
    
    Fixes #24.

--- covid19_sfbayarea/data/__init__.py
+++ covid19_sfbayarea/data/__init__.py
@@ -2,6 +2,7 @@ from typing import Dict, Any
 from . import alameda
 from . import san_francisco
 from . import marin
+from . import napa
 from . import sonoma
 from . import solano
 from . import san_mateo
@@ -11,7 +12,7 @@ scrapers: Dict[str, Any] = {
     'alameda': alameda,
     # 'contra_costa': None,
     'marin': marin,
-    # 'napa': None,
+    'napa': napa,
     'san_francisco': san_francisco,
     'san_mateo': san_mateo,
     'santa_clara': santa_clara,
---
+++ covid19_sfbayarea/data/arcgis.py
@@ -0,0 +1,105 @@
+import requests
+from typing import Any, Dict, Generator
+from urllib.parse import urljoin
+from .errors import BadRequest
+
+
+class ArcGisFeatureServer:
+    """
+    A thin wrapper around ArcGIS's FeatureServer API. The capabilities are
+    pretty limited, but it gives us a useful wrapper for the data-focused parts
+    of the API that we need.
+
+    Parameters
+    ----------
+    base_url : str
+        The base URL of the server, including the "Unique Service ID". Example:
+        ``'https://services1.arcgis.com/Ko5rxt00spOfjMqj'``
+    """
+    def __init__(self, base_url: str):
+        if not base_url.endswith('/'):
+            base_url += '/'
+        self.base_url = base_url
+        self.service_base_url = urljoin(base_url, 'ArcGIS/rest/services/')
+
+    def _build_query_url(self, service: str, table_id: int) -> str:
+        query_path = f'{service}/FeatureServer/{table_id}/query'
+        return urljoin(self.service_base_url, query_path)
+
+    def query(
+        self,
+        service: str,
+        table_id: int = 0,
+        *,  # Parameters below must be specified as keywords. -----------------
+        where: str = '1=1',
+        outFields: str = None,
+        groupByFieldsForStatistics: str = None,
+        orderByFields: str = None,
+        **params: Any
+     ) -> Generator[Dict, None, None]:
+        """
+        Query a table/layer from an ArcGIS Feature Service. This will yield
+        the ``attributes`` field of each result record.
+
+        ArcGIS queries normally require a ``where`` clause, but this method
+        provides a default ``where`` that selects all rows to simplify some
+        queries.
+
+        The parameters of this method don't follow typical python convention
+        because they correspond directly to ArcGIS ``query`` API parameters.
+        Only a subset of commonly used parameters are explicitly listed here
+        for code completion support. For a full reference, see:
+            https://developers.arcgis.com/rest/services-reference/query-feature-service-layer-.htm
+
+        Parameters
+        ----------
+        service : str
+            The name of the service to query, e.g. ``'CaseDataDemographics'``.
+        table_id : int
+            The table/layer ID to query from the service. These IDs are usually
+            sequential and start from 0.
+        where : str
+        outFields : str
+        groupByFieldsForStatistics : str
+        orderByFields : str
+        **params
+            Additional query parameters accepted by the ArcGIS API. Reference:
+            - https://developers.arcgis.com/rest/services-reference/query-feature-service-layer-.htm
+            - https://developers.arcgis.com/documentation/mapping-apis-and-location-services/data-hosting/services/feature-service/
+
+        Yields
+        ------
+        dict
+            This yields the ``attributes`` field of each resulting feature.
+            (Features are GeoJSON, but since we don't care about the geometry,
+            we skip over it and just yield the data.)
+        """
+        url = self._build_query_url(service, table_id)
+        next_params = params.copy()
+        next_params.update({
+            'f': 'json',
+            'returnGeometry': False,
+            'where': where,
+            'outFields': outFields,
+            'groupByFieldsForStatistics': groupByFieldsForStatistics,
+            'orderByFields': orderByFields
+        })
+
+        while True:
+            response = requests.get(url, params=next_params)
+            data = response.json()
+            if 'error' in data:
+                message = data['error']['message']
+                if 'details' in data['error']:
+                    details = ' '.join(data['error']['details'])
+                    message = f'{message} ({details})'
+                raise BadRequest(message, response=response)
+
+            for feature in data['features']:
+                yield feature['attributes']
+
+            if not data.get('exceededTransferLimit', False):
+                return
+
+            offset = next_params.get('resultOffset', 0) + len(data['features'])
+            next_params['resultOffset'] = offset
