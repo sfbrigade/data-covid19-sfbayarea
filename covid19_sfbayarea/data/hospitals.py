323684c7bae59140351dbe2e1404350d6a292c58
Rob Brackett <rob@robbrackett.com>
2021-02-03 22:52:19 -0800

    Add deaths timeseries for San Mateo (#177)
    San Mateo doesn't publish a timeseries for deaths, so we now source that information from the state's data portal. As part of doing so, we've added a lightweight abstraction for querying CKAN, which simplifies some of the hospital data code (since it also uses the state's data portal).
    
    Fixes #171.

--- covid19_sfbayarea/data/hospitals.py
+++ covid19_sfbayarea/data/hospitals.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 import logging
-import requests
 from datetime import datetime
 from dateutil import tz
 from dateutil.parser import parse
 from typing import Any, Dict, List, Union
 
 from covid19_sfbayarea.utils import friendly_county
+from .ckan import Ckan
 
 # This module fetches COVID-19 hospital data from the CA.gov open data portal.
 # The input data is fetched from an API endpoint, and appears to be updated at
@@ -18,7 +18,6 @@ from covid19_sfbayarea.utils import friendly_county
 # URLs and APIs
 HOSPITALS_LANDING_PAGE = "https://data.ca.gov/dataset/covid-19-hospital-data#"
 CAGOV_BASEURL = "https://data.ca.gov"
-CAGOV_API = "/api/3/action/datastore_search"
 HOSPITALS_RESOURCE_ID = "42d33765-20fd-44b8-a978-b083b7542225"
 RESULTS_LIMIT = 200
 
@@ -127,7 +126,6 @@ def process_data(series: List, counties: List) -> Dict:
 def get_timeseries(counties: List) -> Dict:
     """Fetch all pages of timeseries data from API endpoint"""
     timeseries_data: Dict[Any, Any] = {}
-    timeseries_raw: List[Dict[str, Any]] = []
 
     now = datetime.now(tz.tzutc()).isoformat(timespec="minutes")
 
@@ -137,84 +135,29 @@ def get_timeseries(counties: List) -> Dict:
     timeseries_data["source_url"] = HOSPITALS_LANDING_PAGE
     timeseries_data["meta_from_baypd"] = BAYPD_META
 
-    # Call may be made without params on subsequent calls
-    params: Dict[str, Union[int, str]] = {
-        "resource_id": HOSPITALS_RESOURCE_ID,
-        "limit": RESULTS_LIMIT
-    }
+    state_api = Ckan(CAGOV_BASEURL)
+    data_raw = state_api.data(HOSPITALS_RESOURCE_ID,
+                              limit=RESULTS_LIMIT,
+                              yield_meta=True)
+    meta = next(data_raw)
+    records = list(data_raw)
 
-    url = CAGOV_BASEURL + CAGOV_API
+    timeseries_data["meta_from_source"] = []
+    for field in meta["fields"]:
+        if field.get("id") == "todays_date":
+            field = {
+                "info": {
+                    "notes": "Report date",
+                    "type_override": "date",
+                    "label": "Date"
+                },
+                "type": "date",
+                "id": "date"
+            }
 
-    try:
-        # Handle the pagination
-        while True:
-            # pass params if we don't have timeseries data yet
-            if not timeseries_raw:
-                r = requests.get(url, params=params)
+        timeseries_data["meta_from_source"].append(field)
 
-            else:
-                r = requests.get(url)
+    # standardize the format of the data and key it by county name
+    timeseries_data["series"] = process_data(records, counties)
 
-            r.raise_for_status()
-            results = r.json().get("result")
-            total = int(results.get("total"))
-
-            # Get notes only on the first pull
-            if not timeseries_data.get("meta_from_source"):
-                notes = results.get("fields")
-                timeseries_data["meta_from_source"] = notes
-
-            else:
-                pass
-
-            records = results.get("records")
-            timeseries_raw.extend(records)
-
-            results_count = len(timeseries_raw)
-            logger.info(f"Got {results_count} results out of {total} ...")
-            more = results.get("_links").get("next")
-
-            # Don't ask for more pages than there are
-            if more and results_count < total:
-                url = CAGOV_BASEURL + more
-
-            else:
-                break
-        logger.info("Collected all pages")
-
-        # standardize the format of the data and key it by county name
-        timeseries_data["series"] = process_data(timeseries_raw, counties)
-
-        # reflect renaming of the date field in the metadata
-        updated_date_meta = {
-            "info": {
-                "notes": "Report date",
-                "type_override": "date",
-                "label": "Date"
-            },
-            "type": "date",
-            "id": "date"
-        }
-
-        current_meta = timeseries_data["meta_from_source"]
-        updated_meta: Dict = {"meta_from_source": []}
-
-        for entry in current_meta:
-            if entry.get("id") == "todays_date":
-                index = current_meta.index(entry)
-                updated_meta["meta_from_source"].insert(
-                    index, updated_date_meta
-                )
-            else:
-                updated_meta["meta_from_source"].append(entry)
-
-        timeseries_data.update(updated_meta)
-
-    except AttributeError:
-        logger.exception("Error parsing response")
-
-    except requests.exceptions.RequestException:
-        logger.exception("Error fetching from API")
-
-    finally:
-        return timeseries_data
+    return timeseries_data
